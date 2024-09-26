df46b794b2c8d4dd91b30baad83ca2f9872ba0b7
vladyslav <vladyslav.slovak@elev.ga.ntig.se>
2024-09-26 11:18:38 +0200

    snyggat till  bubbleSort

--- bubbleSort.py
+++ bubbleSort.py
@@ -5,32 +5,30 @@ r=100
 i=0
 
 while (i <n):
-    var=random.randint(0,r)
-
-    if var not in intList:
-        intList.append(var)
-        i+=1
+   var=random.randint(0,r)
+   if var not in intList:
+      intList.append(var)
+      i+=1
 
 print(intList)
 
 def bubbleSort():
- x= intList
- print("sorting")
- sorting = 1
- iterator=0
- while(sorting):
-   for i in range(0,n-1):
-      if x[i]>x[i+1]:
-         y=x[i+1]
-         x[i+1]=x[i]
-         x[i]=y
-         iterator=0
-      else:
-       
-         iterator+=1
-      if iterator==10:
-       sorting=False
+   x= intList
+   print("sorting")
+   sorting = 1
+   iterator=0
+   while(sorting):
+      for i in range(0,n-1):
+         if x[i]>x[i+1]:
+            y=x[i+1]
+            x[i+1]=x[i]
+            x[i]=y
+            iterator=0
+         else:
+            iterator+=1
+         if iterator==10:
+            sorting=False
 
- print(x)    
+   print(x)    
 
 bubbleSort()
